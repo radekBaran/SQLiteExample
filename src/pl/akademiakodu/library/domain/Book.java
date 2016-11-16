48e2dedf9c88c0a0621da56afbf9d513c1c1f351
Radoslaw Baranan <th3erm4n@gmail.com>
2016-11-16 14:02:14 +0100

    added Book to package domain

---
+++ src/pl/akademiakodu/library/domain/Book.java
@@ -0,0 +1,70 @@
+package pl.akademiakodu.library.domain;
+
+public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private int pages;
+
+
+    public Book(int id, String title, String author, int pages) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", pages=" + pages +
+                '}';
+    }
+}
